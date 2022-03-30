211cbf6fa6e43f0d3c8389d62b9e6d7757f2dba4
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-03-30 13:18:20 +0200

    new task

--- 06_case_study_1.py
+++ 06_case_study_1.py
@@ -1,6 +1,19 @@
+
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
-path ='data/amzn_us_d.csv'
+path = 'data/amzn_us_d.csv'
 
-df = pd.read_csv(path)
+df = pd.read_csv(path, index_col=0)
+
+close = df['Zamkniecie']
+
+close = close['2010-01-01':]
+# plot
+# close.plot()
+close.plot(title='Amazon', logy=True)
+plt.savefig('./charts/close.png', format='png')
+
+# export to csv
+close.to_csv('data/close_amzn.csv', header=['close'])
