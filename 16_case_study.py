1585300033abd4ce73bfb5a80d92c53c76b66fe0
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-04-03 20:05:40 +0200

    new task

---
+++ 16_case_study.py
@@ -0,0 +1,20 @@
+
+import pandas as pd
+import numpy as np
+df_raw = pd.read_clipboard()
+columns = [col for col in df_raw.columns]
+print(columns)
+# %%
+df = df_raw.drop(['Czas ', '1r 3m'], axis=1)
+df['Wolumen '] = df['Wolumen '].apply(lambda s: s.replace(' ', ''))\
+    .apply(lambda s: int(s))
+df['Obrót '] = df['Obrót '].apply(lambda s: s.replace(' ', ''))\
+    .apply(lambda s: int(s))
+
+df['Udział '] = df['Udział '].apply(lambda s: s.replace('%', ''))\
+    .apply(lambda s: float(s))
+# df['Zmiana '] = df['Zmiana '].apply(lambda s: str(s))
+df['Zmiana '] = df['Zmiana '].apply(lambda s: s.replace('%', ''))\
+    .apply(lambda s: s.replace('(', ''))\
+    .apply(lambda s: s.replace(')', ''))\
+    .apply(lambda s: float(s))
