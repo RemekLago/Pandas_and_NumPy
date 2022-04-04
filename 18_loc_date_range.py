34ece06d7778bbd81fa96d0aab8cd34b8a60eb1f
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-04-04 11:55:30 +0200

    new task

---
+++ 18_loc_date_range.py
@@ -0,0 +1,13 @@
+
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame(np.random.randn(31, 5),
+                  columns=list('abcde'),
+                  index=pd.date_range(start='2019-01-01', periods=31))
+idx = df.index
+
+day = df.loc['2019-01-02']
+week = df.loc['2019-01-01':'2019-01-07']
+after_15 = df.loc['2019-01-15':]
+before_15 = df.loc[:'2019-01-15']
