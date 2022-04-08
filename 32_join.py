df3c221c5184c6292dab61125f19dfa33d2fc788
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-04-08 12:57:09 +0200

    new task

---
+++ 32_join.py
@@ -0,0 +1,20 @@
+
+import pandas as pd
+import numpy as np
+left = pd.DataFrame(np.random.rand(10, 3),
+                    index=pd.date_range('2019-01-01', periods=10),
+                    columns=list('abc'))
+right = pd.DataFrame(np.random.rand(10, 3),
+                     index=pd.date_range('2019-01-04', periods=10),
+                     columns=list('def'))
+
+# left
+left_join = left.join(right, how='left')
+
+# right
+right_join = left.join(right, how='right')
+
+# inner
+inner_join = left.join(right, how='inner')
+# outer
+outer_join = left.join(right, how='outer')
